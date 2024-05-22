0c8f64299f8f48e316ecb1ad7b636061dfd85e24
thet <apple@appurunoiMac.local>
2024-05-22 18:35:29 +0900

    todocopy

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -111,7 +111,11 @@
                                              </ul>
                                          </div>
                                      </div>   
-                                     <h6 class="card-subtitle ms-1 mb-1 text-muted">{{ $todo->created_at }}</h6>                                                               
+                                     <h6 class="card-subtitle ms-1 mb-1 text-muted">{{ $todo->created_at }}</h6> 
+                                     <div class="d-flex flex-wrap mx-1 mb-1">
+                                         
+                                     
+
                                  </div>                                
                              </div>
                          @endforeach
